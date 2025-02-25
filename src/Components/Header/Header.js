7149da3c3a0098d57a84c5f221a57a8f6c836aac
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-02-25 06:41:50 -0800

    update

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import Search from "./Search/Search";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-rose-500 flex">
+      <Search />
+    </div>
+  );
+};
+
+export default Header;
