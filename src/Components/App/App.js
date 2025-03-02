06a2eb166009f07fc638f5d112ccf0331722b99f
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-02 12:03:46 -0800

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,20 @@
 import Header from "../Header/Header";
 import Shelves from "../Shelves/Shelves";
-
+let booksList = [];
 export function Getdata(query) {
   fetch(`https://www.dbooks.org/api/search/${query}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      booksList = data["books"];
     });
+  console.log(booksList);
 }
 
 const App = () => {
   return (
     <>
       <Header />
-      <Shelves />
+      <Shelves booksList={booksList} />{" "}
     </>
   );
 };
--- src/Components/Shelves/Shelves.js
+++ src/Components/Shelves/Shelves.js
@@ -1,5 +1,17 @@
-const Shelves = () => {
-  return <div className=" bg-green-400 w-full">s</div>;
+const Shelves = ({ booksList }) => {
+  return (
+    <>
+      {" "}
+      {booksList.map((elem, index) => {
+        return (
+          <div className=" bg-gray-500 w-3/12 flex flex-col " key={index}>
+            <img src={elem["image"]} alt={elem["subtitle"]} />
+            <p>{elem["title"]}</p>
+          </div>
+        );
+      })}{" "}
+    </>
+  );
 };
 
 export default Shelves;
